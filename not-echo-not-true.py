1f95d906c90a28671de124c1e84ffb8d617107c3
Luis Adha <40796508+luisadha@users.noreply.github.com>
2025-07-21 23:28:55 +0700

    Update not-echo-not-true.py

--- not-echo-not-true.py
+++ not-echo-not-true.py
@@ -5,7 +5,7 @@ import sys
 import time
 import shutil
 
-__version__ = "v1.4.7-nightly-20250718"
+__version__ = ""1.4.32-nightly"
 
 def center_text(text):
     width = shutil.get_terminal_size((80, 20)).columns
