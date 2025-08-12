b22acdbea1a035a650ed67725626aadf4914b2ca
Luis Adha <40796508+luisadha@users.noreply.github.com>
2025-08-13 05:10:53 +0700

    Improvement completions

--- completions/nene.completions.bash
+++ completions/nene.completions.bash
@@ -5,24 +5,21 @@ _nene() {
 
   if [[ "$COMP_CWORD" -eq 1 ]]; then
     COMPREPLY=( $(compgen -W "$(nene flags)" -- "$word") )
+
   elif [[ "$prev" == "-R" ]]; then
-    local cache_file="$PREFIX/tmp/nene-endpoints-cache"
-    local now=$(date +%s)
-
-    # Cek apakah file cache lebih lama dari 10 menit
-    if [[ ! -f $cache_file || $((now - $(stat -c %Y "$cache_file"))) -gt 600 ]]; then
-      mapfile -t urls < <(curl -s "https://luisadha.my.id/api" | jq -r '.[]')
-      : > "$cache_file"  # Kosongkan cache
-      for url in "${urls[@]}"; do
-        endpoint=$(basename "$url")
-        if command -v "$endpoint" &>/dev/null; then
-          echo "$endpoint" >> "$cache_file"
-        fi
-      done
+    local list_file="$HOME/.local/share/nene/pkg-installed"
+    if [[ -f "$list_file" ]]; then
+      mapfile -t completions < "$list_file"
+      COMPREPLY=( $(compgen -W "${completions[*]}" -- "$word") )
     fi
+  elif [[ "$prev" == "-S" ]]; then
+    local list_file="$HOME/.local/share/nene/update-lists"
 
-    mapfile -t completions < "$cache_file"
-    COMPREPLY=( $(compgen -W "${completions[*]}" -- "$word") )
+    if [[ -f "$list_file" ]]; then
+      mapfile -t completions < "$list_file"
+      COMPREPLY=( $(compgen -W "${completions[*]}" -- "$word") )
+    fi
   fi
 }
+
 complete -F _nene nene
