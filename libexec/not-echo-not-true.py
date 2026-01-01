311ce55c116acbecea3a5cee025264ba7ff09505
Tenet <40796508+luisadha@users.noreply.github.com>
2026-01-01 08:50:03 +0700

    Update not-echo-not-true.py

--- libexec/not-echo-not-true.py
+++ libexec/not-echo-not-true.py
@@ -4,7 +4,7 @@ import sys
 import time
 import shutil
 
-__version__ = "1.6.1-libnene"
+__version__ = "1.5.97-preview"
 
 def center_text(text):
     width = shutil.get_terminal_size((80, 20)).columns
