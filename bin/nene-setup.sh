aff5bc728bb9349efbbb2a0cc852c4548aaa2dbe
Tenet <40796508+luisadha@users.noreply.github.com>
2026-01-01 17:42:01 +0700

    Add shebang and install dependencies in setup script
    Added shebang and installation of dependencies.

--- bin/nene-setup.sh
+++ bin/nene-setup.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/bash
 AUTHOR="luisadha"
+pkg install -y yq make
 cd "$HOME"/.basher/cellar/packages/$AUTHOR/nene
 echo "Installing.. dependencies"
 for pkg in $(yq '.packages[]' requirements.yml); do
