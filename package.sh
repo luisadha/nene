892024e4f1b17db755ae94a5d0d60a956ebe33ce
TENET <40796508+luisadha@users.noreply.github.com>
2026-01-05 18:34:15 +0700

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-BINS=bin/nene:bin/nene-setup.sh
+BINS=bin/nene:bin/bin/nene-missing-deps.sh
 #DEPS=user1/repo1:user2/repo2
 BASH_COMPLETIONS=completions/nene.completion.bash
 ZSH_COMPLETIONS=completions/nene.completion.zsh
